f567ba98ead824810e5256da55fb02727c62d141
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-18 13:57:45 -0600

    Add more tests for immutability.

---
+++ test/immutable_spec.js
@@ -0,0 +1,58 @@
+import {expect} from 'chai';
+import {List, Map} from 'immutable';
+
+describe('immutability', () => {
+
+  describe('a number', () => {
+
+    function increment(currentState) {
+      return currentState + 1;
+    }
+
+    it('is immutable', () => {
+      let state = 42;
+      let nextState = increment(state);
+
+      expect(nextState).to.equal(43);
+      expect(state).to.equal(42);
+    });
+
+  });
+
+  describe('a tree', () => {
+    function addMovie(currentState, movie) {
+      return currentState.set(
+        'movies',
+        currentState.get('movies').push(movie)
+      );
+    }
+
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+      let nextState = addMovie(state, 'Sunshine');
+
+
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
+    });
+
+  });
+
+});
+
+function addMovie(currentState, movie) {
+  return currentState.update('movies', movies => movies.push(movie));
+}
