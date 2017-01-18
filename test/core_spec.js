64ca536dd198b7b579926c0f80660642706ad678
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-18 13:59:43 -0600

    Import List for core.js.

--- test/core_spec.js
+++ test/core_spec.js
@@ -16,6 +16,15 @@ describe('application logic', () => {
       }));
     });
 
+    it('converts to immutable', () => {
+      const state = Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+
   });
 
 });
