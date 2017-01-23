5869085c63985979756d56658f0a3864a256fb74
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-23 15:29:42 -0600

    Setup initial entries and store dispatch.

--- src/server.js
+++ src/server.js
@@ -9,5 +9,7 @@ export default function startServer(store) {
 
 	io.on('connection', (socket) => {
 		socket.emit('state', store.getState().toJS());
+		socket.on('action', store.dispatch.bind(store));
 	});
+
 }
