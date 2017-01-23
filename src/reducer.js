4ff4cd3f9e564cecd7b597fc35473545fe09d699
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-23 12:52:42 -0600

    Change vote functionality to only change update vote state.

--- src/reducer.js
+++ src/reducer.js
@@ -3,12 +3,13 @@ import {setEntries, next, vote, INITIAL_STATE} from './core';
 export default function reducer(state = INITIAL_STATE, action) {
 	// figure out which function to call and call it
 	switch(action.type) {
-		case 'SET_ENTRIES':
-			return setEntries(state, action.entries);
-		case 'NEXT':
-			return next(state);
-		case 'VOTE':
-			return vote(state, action.entry);
+	case 'SET_ENTRIES':
+		return setEntries(state, action.entries);
+	case 'NEXT':
+		return next(state);
+	case 'VOTE':
+		return state.update('vote',
+			voteState => vote(voteState, action.entry));
 	}
 	return state;
 }
